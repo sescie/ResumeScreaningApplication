6fd4ef50da1ece096270f8bd7d722c6eda96a545
taoneswa kasirai <61433155+taoneswa@users.noreply.github.com>
2023-10-17 15:22:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,61 @@
-import streamlit as st
-import pickle
-import re
-import nltk
+import streamlit as st 
+import PyPDF2
+import pdfplumber
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-nltk.download('punkt')
-nltk.download('stopwords')
+st.title("Candidate Selection Tool")
 
-#loading models
-clf = pickle.load(open('clf.pkl','rb'))
-tfidfd = pickle.load(open('tfidf.pkl','rb'))
+st.subheader("NLP Based Resume Screening")
 
-def clean_resume(resume_text):
-    clean_text = re.sub('http\S+\s*', ' ', resume_text)
-    clean_text = re.sub('RT|cc', ' ', clean_text)
-    clean_text = re.sub('#\S+', '', clean_text)
-    clean_text = re.sub('@\S+', '  ', clean_text)
-    clean_text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_text)
-    clean_text = re.sub(r'[^\x00-\x7f]', r' ', clean_text)
-    clean_text = re.sub('\s+', ' ', clean_text)
-    return clean_text
-# web app
-def main():
-    st.title("Resume Screening App")
-    uploaded_file = st.file_uploader('Upload Resume', type=['txt','pdf'])
+st.caption("Aim of this project is to check whether a candidate is qualified for a role based his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
 
-    if uploaded_file is not None:
-        try:
-            resume_bytes = uploaded_file.read()
-            resume_text = resume_bytes.decode('utf-8')
-        except UnicodeDecodeError:
-            # If UTF-8 decoding fails, try decoding with 'latin-1'
-            resume_text = resume_bytes.decode('latin-1')
+uploadedJD = st.file_uploader("Upload Job Description", type="pdf")
 
-        cleaned_resume = clean_resume(resume_text)
-        input_features = tfidfd.transform([cleaned_resume])
-        prediction_id = clf.predict(input_features)[0]
-        st.write(prediction_id)
+uploadedResume = st.file_uploader("Upload resume",type="pdf")
 
-        # Map category ID to category name
-        category_mapping = {
-            15: "Java Developer",
-            23: "Testing",
-            8: "DevOps Engineer",
-            20: "Python Developer",
-            24: "Web Designing",
-            12: "HR",
-            13: "Hadoop",
-            3: "Blockchain",
-            10: "ETL Developer",
-            18: "Operations Manager",
-            6: "Data Science",
-            22: "Sales",
-            16: "Mechanical Engineer",
-            1: "Arts",
-            7: "Database",
-            11: "Electrical Engineering",
-            14: "Health and fitness",
-            19: "PMO",
-            4: "Business Analyst",
-            9: "DotNet Developer",
-            2: "Automation Testing",
-            17: "Network Security Engineer",
-            21: "SAP Developer",
-            5: "Civil Engineer",
-            0: "Advocate",
-        }
-
-        category_name = category_mapping.get(prediction_id, "Unknown")
-
-        st.write("Predicted Category:", category_name)
+click = st.button("Process")
 
 
 
-# python main
-if __name__ == "__main__":
-    main()
+try:
+    global job_description
+    with pdfplumber.open(uploadedJD) as pdf:
+        pages = pdf.pages[0]
+        job_description = pages.extract_text()
+
+except:
+    st.write("")
+    
+    
+try:
+    global resume
+    with  pdfplumber.open(uploadedResume) as pdf:
+        pages = pdf.pages[0]
+        resume = pages.extract_text()
+except:
+    st.write("")
+    
+#logic
+def getResult(JD_txt,resume_txt):
+    content = [JD_txt,resume_txt]
+
+    cv = CountVectorizer()
+
+    matrix = cv.fit_transform(content)
+
+    similarity_matrix =  cosine_similarity(matrix)
+
+    match = similarity_matrix[0][1] * 100
+
+    return match
+
+
+#button 
+
+if click:
+    match = getResult(job_description,resume)
+    match = round(match,2)
+    st.write("Match Percentage: ",match,"%")
+
+st.caption(" ~ made by siddhraj")
